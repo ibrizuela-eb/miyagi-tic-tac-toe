dbe9fae39eab6b1fee8ef0b179067a22d7bd3fdb
ibrizuela-eb <ibrizuela@eventbrite.com>
2022-03-07 13:00:27 -0300

    Ignore unnecessary files and disable wildcards

--- src/test/kotlin/PlayerTest.kt
+++ src/test/kotlin/PlayerTest.kt
@@ -1,4 +1,4 @@
-import org.junit.jupiter.api.Assertions.*
+import org.junit.jupiter.api.Assertions.* // ktlint-disable no-wildcard-imports
 import org.junit.jupiter.api.Test
 
 internal class PlayerTest {
