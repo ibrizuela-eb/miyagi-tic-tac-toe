f088996f18af13ab613e489aab94fdbbe0da4fd7
ibrizuela-eb <ibrizuela@eventbrite.com>
2022-03-11 17:43:12 -0300

    Test for raffle function are failing

--- src/main/kotlin/Game.kt
+++ src/main/kotlin/Game.kt
@@ -1,3 +1,30 @@
 class Game() {
     var board: String = "         "
+	val players: MutableList<Player> = mutableListOf()
+
+	fun raffleTeamForPlayers(playerOneName: String, playerTwoName: String): Map<String, String> {
+		/* This method receives 2 names of player and assigns a team to each one
+		Inputs
+		 	"Nacho", "Mauri"
+		Output
+			{"Nacho"="X", "Mauri"="O"}
+		*/
+		return mapOf(playerOneName to "X", playerTwoName to "O")
+	}
+
+	fun createPlayers(playerOneName: String, playerTwoName: String): MutableList<Player> {
+		val teamRaffled = this.raffleTeamForPlayers(
+			playerOneName = playerOneName,
+			playerTwoName = playerTwoName
+		)
+		val playerOne: Player = Player(
+			name = playerOneName,
+			team = teamRaffled[playerOneName]
+		)
+		val playerTwo: Player = Player(
+			name = playerTwoName,
+			team = teamRaffled[playerTwoName]
+		)
+		return mutableListOf(playerOne, playerTwo)
+	}
 }
